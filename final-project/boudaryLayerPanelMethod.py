7a354ad970e85c1552e0b29649cfa46ce95da823
Ian <icarr@gwu.edu>
2014-04-26 19:18:51 -0400

    Changes to integration method
    not sure if it’s actually correct

--- final-project/boudaryLayerPanelMethod.py
+++ final-project/boudaryLayerPanelMethod.py
@@ -64,7 +64,7 @@ def definePanels(N,xp,yp):
         
     return panel
     
-N = 20                      #  number of panels <----------------
+N = 30                      #  number of panels <----------------
 panel = definePanels(N,xp,yp)   # discretization of the geometry into panels
 
 # ------------ defining freestream conditions -----------
@@ -215,39 +215,40 @@ plt.show()
  
 # ------------ BEGINNING OF NEW CODE ----------
 
-# creating the running surface coordinate system
-for i in range(N):
-    print panel[i].xc
+# as a first round it is necessary to split up the code into two parts
+# one for the extrados (top) and one for in the intrados (bottom)
 
-sExt = np.zeros(N/2)
-for i in range(N/2):
+N2 = N/2 # looping variable for one side of the airfoil
+
+# creating the running surface coordinate system
+sExt = np.zeros(N2)
+for i in range(N2):
     if i==0:
         sExt[i]=0
     if i>0:
         sExt[i] = sExt[i-1]+0.5*panel[i].length + 0.5*panel[i-1].length
 
+# creating separate velocity arrays
+vtExt = [-p.vt for p in panel if p.loc=='extrados']
+vtInt = [p.vt for p in panel if p.loc=='intrados']
+
+# flipping velocity arrays
+vtExt = vtExt[::-1]
+
 # calculatig velocity gradient
-dvdxInt = np.gradient([-p.vt for p in panel if p.loc=='intrados'])
-dvdxExt = np.gradient([p.vt for p in panel if p.loc=='extrados'])
+dvdxInt = np.gradient(vtExt)
+dvdxExt = np.gradient(vtInt)
 dvdx = np.gradient([p.vt for p in panel])
 
 # flipping the velocity gradient arrays
-dvdxExt = dvdxExt[::-1]
+#dvdxExt = dvdxExt[::-1]
 
 # plotting
 plt.figure(figsize=(10,6))
 plt.grid(True)
 plt.xlabel('Airfoil Surface',fontsize=16)
 plt.ylabel('Velocity Gradient',fontsize=16)
-plt.plot([p.xc for p in panel if p.loc=='intrados'], dvdxInt,'-ro')
-plt.plot([p.xc for p in panel if p.loc =='extrados'], dvdxExt,'-bo')
-plt.show()
-
-plt.figure(figsize=(10,6))
-plt.grid(True)
-plt.xlabel('Airfoil Surface',fontsize=16)
-plt.ylabel('Velocity Gradient',fontsize=16)
-plt.plot(sExt, dvdxExt,'-bo')
+plt.plot(sExt, dvdxExt,'-ro')
 plt.show()
 
 # calculating Reynolds number based on freestream velocity
@@ -257,41 +258,91 @@ mu = 1.9*10**-5             # dynamic viscosity of air kg/ms
 nu = mu/rho                 # kinematic viscosity
 L = max(xp)-min(xp)
 Re = rho*Uinf*L/mu
+print Re
 
 # calculating integral in the momentum thickness equation
-intVe = np.zeros_like(dvdx,dtype=float) # integral in theta calculation
-theta = np.zeros_like(intVe,dtype=float)
+intVe = np.zeros_like(sExt)# integral in theta calculation
+theta = np.zeros_like(intVe)
 
 # intermediate calcualtion of integral
-for i in range(N): 
-    intVe[i] = integrate.trapz([p.vt**5 for p in panel],[panel[0].xc,panel[i].xc])
+for i in range(N2):
+    A = (vtExt[i]**5)*sExt[i]
+    intVe[i] = sum(intVe)+A
+    print intVe
 
 # calcating momentum thickness
-for i in range(N):
-    theta[i] = np.sqrt((0.45/panel[i].vt**6)*intVe[i])
-    if theta[i] == 0: 
-        theta[i]=np.sqrt((0.075*mu)/(rho*dvdx[(len(dvdx)/2)]))
+for i in range(N2):
+    theta[i] = np.sqrt(((0.45*nu)/vtExt[i]**6)*intVe[i])
         
+theta[0]=np.sqrt((0.075*nu)/(dvdxExt[0]))
     
 # calculating the pressure gradient parameter
-lam = np.empty_like(theta)
-for i in range(N):
-    lam[i] = (rho*theta[i]**2/mu)*(dvdx[i]) 
+lam = np.zeros_like(theta)
+for i in range(N2):
+    lam[i] = ((rho*theta[i]**2)/mu)*(dvdxExt[i])
     
 # calculating shape factor H from lambda (lam)
 H = np.zeros_like(lam)
-for i in range(N):
+for i in range(N2):
     if lam[i]>0 and lam[i]<0.1:
         H[i] = 2.61-3.75*lam[i]+5.24*lam[i]**2
     if lam[i]>-0.1 and lam[i]<0:
-        H[i] = 2.008+(0.0731/(lam[i]+0.14))
+        H[i] = 2.088+(0.0731/(lam[i]+0.14))
         
 # calculating displacement thickness
 disp = H*theta
-plt.figure(figsize=(10,6))
-plt.title('Displacement Thickness')
-plt.plot([p.xc for p in panel],disp)
+
+# creating separate position arrays for plotting
+xcExt = [p.xc for p in panel if p.loc=='extrados']
+xcInt = [p.xc for p in panel if p.loc=='intrados']
+ycExt = [p.yc for p in panel if p.loc=='extrados']
+ycInt = [p.yc for p in panel if p.loc=='intrados']
+
+betaExt = [p.beta for p in panel if p.loc=='extrados']
+betaInt = [p.beta for p in panel if p.loc=='intrados']
+
+
+# adding the height of the displacement thickness to the airfoil
+for i in range(N2):
+    xDisp = disp[i]*np.cos(betaExt[i])+xcExt
+    yDisp = disp[i]*np.sin(betaExt[i])+ycExt
+    
+for i in range(N2):
+    xTheta = theta[i]*np.cos(betaExt[i])+xcExt
+    yTheta = theta[i]*np.sin(betaExt[i])+ycExt    
+    
+# plotting the discretized geometry
+valX,valY = 0.1,0.2
+xmin,xmax = min([p.xa for p in panel]),max([p.xa for p in panel])
+ymin,ymax = min([p.ya for p in panel]),max([p.ya for p in panel])
+xStart,xEnd = xmin-valX*(xmax-xmin),xmax+valY*(xmax-xmin)
+yStart,yEnd = ymin-valY*(ymax-ymin),ymax+valY*(ymax-ymin)
+size = 10
+plt.figure(figsize=(size,(yEnd-yStart)/(xEnd-xStart)*size))
+plt.grid(True)
 plt.xlabel('x',fontsize=16)
-plt.ylabel('Disp',fontsize=16)
-plt.show()
+plt.ylabel('y',fontsize=16)
+plt.title('Displacement Thickness')
+plt.xlim(xStart,xEnd)
+plt.ylim(yStart,yEnd)
+plt.plot(xp,yp,'k-',linewidth=2)
+plt.plot(xDisp,yDisp,linewidth=2)
+
+# plotting the discretized geometry
+valX,valY = 0.1,0.2
+xmin,xmax = min([p.xa for p in panel]),max([p.xa for p in panel])
+ymin,ymax = min([p.ya for p in panel]),max([p.ya for p in panel])
+xStart,xEnd = xmin-valX*(xmax-xmin),xmax+valY*(xmax-xmin)
+yStart,yEnd = ymin-valY*(ymax-ymin),ymax+valY*(ymax-ymin)
+size = 10
+plt.figure(figsize=(size,(yEnd-yStart)/(xEnd-xStart)*size))
+plt.grid(True)
+plt.xlabel('x',fontsize=16)
+plt.ylabel('y',fontsize=16)
+plt.title('Momentum Thickness')
+plt.xlim(xStart,xEnd)
+plt.ylim(yStart,yEnd)
+plt.plot(xp,yp,'k-',linewidth=2)
+plt.plot(xTheta,yTheta,linewidth=2)
+
 
